4a062a7f4d3316c84a0bfce911b1f228211aa649
0mhz <87370196+0mhz@users.noreply.github.com>
2024-06-19 08:49:41 +0200

    Update process_unique_exifs_xargs_call.sh

--- process_unique_exifs_xargs_call.sh
+++ process_unique_exifs_xargs_call.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /Users/shinka/Userscripts/process_unique_exifs_xargs_main.sh
+source /####/process_unique_exifs_xargs_main.sh
 
 function processPhoto() {
   #echo "Arguments received by processPhoto: $@"
@@ -16,13 +16,13 @@ function processPhoto() {
       FILE_MOD_TIME=$(echo "$INFO" | grep -E "File Modification Date/Time" | awk '{gsub(/:/,"",$4); gsub(/:/,"",$5); print $4$5"-"substr($6, 1, 11)}' | sed 's/://g' | sed 's/+//g')
       if [ -n "$FILE_MOD_TIME" ] && [ ${#FILE_MOD_TIME} -gt 7 ]; then
 	if [ -n "$FILE_ORI_TIME" ] && [ ${#FILE_ORI_TIME} -gt 7 ]; then
-          NEW_FILE_NAME="LUDWIG_ORI${FILE_ORI_TIME}_MOD${FILE_MOD_TIME}.${IMAGE_EXTENSION}"
+          NEW_FILE_NAME="####_ORI${FILE_ORI_TIME}_MOD${FILE_MOD_TIME}.${IMAGE_EXTENSION}"
 	  # else, i.e., if the original timestamp is available, use it to name the folder
 	  DATE=$(echo "$FILE_ORI_TIME" | grep -oE "^[0-9]+")
           #EXPORT_MSG=" EXPORT with ORIDATE=${DATE} -- DEBUG: FILE_ORI_TIME=${FILE_ORI_TIME} (Length: ${#FILE_ORI_TIME})"
           EXPORT_MSG=" EXPORT with ORIDATE=${DATE}"
 	else
-          NEW_FILE_NAME="LUDWIG_ORI-ISEMPTY_MOD${FILE_MOD_TIME}.${IMAGE_EXTENSION}"
+          NEW_FILE_NAME="####_ORI-ISEMPTY_MOD${FILE_MOD_TIME}.${IMAGE_EXTENSION}"
 	  # If we can't use the original timestamp for the folder name then use the file modification date
 	  DATE=$(echo "$FILE_MOD_TIME" | grep -oE "^[0-9]+")
           #EXPORT_MSG=" EXPORT with MODDATE=${DATE} -- DEBUG: FILE_ORI_TIME=${FILE_ORI_TIME} (Length: ${#FILE_ORI_TIME})"
@@ -43,11 +43,11 @@ function processPhoto() {
     #CAMERA_MODEL=$(echo "$INFO" | grep -i "Camera Model Name" | cut -d ":" -f2 | cut -d ' ' -f3 | sed 's/ //g')
     
     case $CAMERA_MODEL in
-      "LEICA_M10")
+      "####")
         RAW_FILE_NAME=$(echo "$INFO" | grep -e ^Raw\ File\ Name | cut -c 35-99 | sed 's/ /_/g')
         NEW_FILE_NAME="${DATETIME}_${CAMERA_MODEL}_${RAW_FILE_NAME}"
         ;;
-      "LEICA_SL2")
+      "####")
         ORIGINAL_FILE_NAME=$(echo "$INFO" | grep -e ^Original\ File\ Name | cut -c 35-99 | sed 's/ /_/g')
         NEW_FILE_NAME="${DATETIME}_${CAMERA_MODEL}_${ORIGINAL_FILE_NAME}"
         if [[ -z $ORIGINAL_FILE_NAME ]]; then
@@ -55,23 +55,23 @@ function processPhoto() {
           NEW_FILE_NAME="${DATETIME}_${CAMERA_MODEL}.$IMAGE_EXTENSION"
         fi
         ;;
-      "EOS"* | "Canon"*)
+      "####"* | "####"*)
         IMAGE_ID=$(echo "$FILE_NAME" | grep -oE 'LXRW[0-9-]+')
         IMAGE_EXTENSION=$(basename "$FILE_NAME" | awk -F . '{print $NF}')
         CANON_MODEL=$(echo "$INFO" | grep -e "^Canon\ Model" | cut -d ":" -f2 | sed 's/ /_/g; s/-/_/g')
         NEW_FILE_NAME="${DATETIME}${CANON_MODEL}_${IMAGE_ID}.${IMAGE_EXTENSION}"
         ;;
-      "X100T")
+      "####")
         IMAGE_ID=$(echo "$FILE_NAME" | grep -oE '(DSCF|LXA)[0-9]+')
         IMAGE_EXTENSION=$(basename "$FILE_NAME" | awk -F . '{print $NF}')
         NEW_FILE_NAME="${DATETIME}_${CAMERA_MODEL}_${IMAGE_ID}.${IMAGE_EXTENSION}"
         ;;
-      "DMC-LX5")
+      "####")
         IMAGE_ID=$(echo "$FILE_NAME" | grep -oE 'P[0-9]+')
         IMAGE_EXTENSION=$(basename "$FILE_NAME" | awk -F . '{print $NF}')
         NEW_FILE_NAME="${DATETIME}_${CAMERA_MODEL}_${IMAGE_ID}.${IMAGE_EXTENSION}"
         ;;
-      "DC-TZ202")
+      "####")
         IMAGE_ID=$(echo "$FILE_NAME" | grep -oE '(_|P)[0-9]+')
         if [[  $IMAGE_ID == _* ]]; then
           IMAGE_ID=P$(echo "$IMAGE_ID" | grep -oE '[0-9]+')
@@ -79,7 +79,7 @@ function processPhoto() {
         IMAGE_EXTENSION=$(basename "$FILE_NAME" | awk -F . '{print $NF}')
         NEW_FILE_NAME="${DATETIME}_${CAMERA_MODEL}_${IMAGE_ID}.${IMAGE_EXTENSION}"
         ;;
-      "Nexus_5"|"HTC_Desire_601"|"LG-H8"*)
+      "####"|"####"|"####"*)
         IMAGE_EXTENSION=$(basename "$FILE_NAME" | awk -F . '{print $NF}')
         NEW_FILE_NAME="${DATETIME}_${CAMERA_MODEL}.${IMAGE_EXTENSION}"
         ;;
